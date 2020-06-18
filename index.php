7eb18c451668acd3005ae570ad19c224523f2b5b
Momo <mohamedamin.oukkal@gmail.com>
2020-06-18 09:34:44 +0200

    a

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="script.js"></script>
+</head>
+<body>
+    <div id="demo">
+
+        <h2>Let AJAX change this text <?php echo 4+1 ;?></h2>
+        <button type="button" onclick="loadDoc()">Change Content</button>
+      </div>
+      <?php
+    echo 1+1;
+?>
+</body>
+</html>
